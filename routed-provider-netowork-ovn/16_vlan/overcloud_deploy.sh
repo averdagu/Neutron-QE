1454c9a31ec00c1d8a2af97232b74abebc267d77
Alex Katz <akatz@redhat.com>
2021-02-18 18:01:33 +0200

    Fix overcloud deploy script in OVN routed provider network template
    Change-Id: Ieed26f943b879222105d221378c522df648aa4f4

--- routed-provider-netowork-ovn/16_vlan/overcloud_deploy.sh
+++ routed-provider-netowork-ovn/16_vlan/overcloud_deploy.sh
@@ -6,7 +6,7 @@ openstack overcloud deploy \
     --libvirt-type kvm \
     --stack overcloud \
     -r /home/stack/16_vlan/roles/roles_data.yaml \
-    -n /home/stack/16_vlan/network/network-config.yaml \
+    -n /home/stack/16_vlan/network/network_data.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /home/stack/16_vlan/network/network-environment-overrides.yaml \
