1260135f8648350ffda5b0581f8bac441a7c16a9
Maor Blaustein <blue@redhat.com>
2022-06-28 19:50:38 +0300

    Fixes for OSP17 vlan-provider job
    Change-Id: I65666770f7115404559ac0c0059087beeb01906f

--- vlan_provider_network_ovn/OSP17/scripts/overcloud_deploy.sh
+++ vlan_provider_network_ovn/OSP17/scripts/overcloud_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ ! -f "/home/stack/vlan_provider_network_ovn/OSP17/roles_data.yaml" ]]; then
-  openstack overcloud roles generate -o /home/stack/vlan_provider_network_ovn/OSP17/roles_data.yaml Controller Compute Networker
+if [[ ! -f "/home/stack/OSP17/roles_data.yaml" ]]; then
+  openstack overcloud roles generate -o /home/stack/OSP17/roles_data.yaml Controller Compute Networker
 fi
 
 openstack overcloud deploy \
@@ -9,22 +9,22 @@ openstack overcloud deploy \
 --templates /usr/share/openstack-tripleo-heat-templates \
 --libvirt-type kvm \
 --stack overcloud \
--r /home/stack/vlan_provider_network_ovn/OSP17/roles_data.yaml \
--n /home/stack/vlan_provider_network_ovn/OSP17/network/network_data_v2.yaml \
+-r /home/stack/OSP17/roles_data.yaml \
+-n /home/stack/OSP17/network/network_data_v2.yaml \
 --deployed-server \
 -e /home/stack/templates/overcloud-vip-deployed.yaml \
 -e /home/stack/templates/overcloud-networks-deployed.yaml \
 -e /home/stack/templates/overcloud-baremetal-deployed.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/cinder-backup.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/services/neutron-ovn-dvr-ha.yaml \
--e /home/stack/vlan_provider_network_ovn/OSP17/network/network-environment.yaml \
--e /home/stack/vlan_provider_network_ovn/OSP17/roles/nodes-17.yaml \
--e /home/stack/vlan_provider_network_ovn/OSP17/ovn-extras.yaml \
--e /home/stack/vlan_provider_network_ovn/OSP17/l3_fip_qos.yaml \
--e /home/stack/vlan_provider_network_ovn/OSP17/performance.yaml \
--e /home/stack/vlan_provider_network_ovn/OSP17/debug.yaml \
--e /home/stack/vlan_provider_network_ovn/OSP17/hostnames.yaml \
--e /home/stack/vlan_provider_network_ovn/OSP17/set-nova-scheduler-filter.yaml \
+-e /home/stack/OSP17/network/network-environment_v2.yaml \
+-e /home/stack/OSP17/roles/nodes-17.yaml \
+-e /home/stack/OSP17/ovn-extras.yaml \
+-e /home/stack/OSP17/l3_fip_qos.yaml \
+-e /home/stack/OSP17/performance.yaml \
+-e /home/stack/OSP17/debug.yaml \
+-e /home/stack/OSP17/hostnames.yaml \
+-e /home/stack/OSP17/set-nova-scheduler-filter.yaml \
 -e /home/stack/containers-prepare-parameter.yaml \
 --ntp-server clock.redhat.com,time1.google.com,time2.google.com,time3.google.com,time4.google.com \
 --log-file overcloud_install.log &> overcloud_install.log
