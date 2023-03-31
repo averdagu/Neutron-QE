c3f381c17c10d19976a5943bd61811199f68ba7e
Bharath M V <bmv@redhat.com>
2023-03-31 13:24:33 +0530

    Add ntp setting to 16 vlan provider and Add container_prepare yaml file
    Both these things are done to accomodate the UPGRADES DFG job which
    upgrades from 16.2 --> 17.1 for OVN VLAN provider job
    
    Change-Id: If89aa0a30b04e1428603e612a0e249efc2b61396

--- vlan_provider_network_ovn/scripts/16/overcloud_deploy.sh
+++ vlan_provider_network_ovn/scripts/16/overcloud_deploy.sh
@@ -5,6 +5,7 @@ openstack overcloud deploy \
 --templates /usr/share/openstack-tripleo-heat-templates \
 --libvirt-type kvm \
 --stack overcloud \
+--ntp-server clock.corp.redhat.com \
 -r /home/stack/vlan_provider_network_ovn/roles/roles_data-16.yaml \
 -n /home/stack/vlan_provider_network_ovn/network/network-config.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/cinder-backup.yaml \
@@ -14,7 +15,7 @@ openstack overcloud deploy \
 -e /home/stack/vlan_provider_network_ovn/roles/nodes-16.yaml \
 -e /home/stack/vlan_provider_network_ovn/ovn-extras.yaml \
 -e /home/stack/vlan_provider_network_ovn/l3_fip_qos.yaml \
--e /home/stack/vlan_provider_network_ovn/docker-images.yaml \
+-e /home/stack/containers-prepare-parameter.yaml \
 -e /home/stack/vlan_provider_network_ovn/performance.yaml \
 -e /home/stack/vlan_provider_network_ovn/debug.yaml \
 -e /home/stack/vlan_provider_network_ovn/hostnames.yaml \
