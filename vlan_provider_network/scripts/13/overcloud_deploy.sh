ce8ba20b50d8372bf60b0f732a0f80f71a8b42f2
Roman Safronov <rsafrono@redhat.com>
2023-05-24 18:22:14 +0300

    Divide vlan_provide_network templates to 13 and 16
    Current version of vlan_provider_network templates is suitable for
    osp13 but not for osp16. This patch creates tempates suitable for
    osp16 that are intended for usage in upgrade jobs.
    
    Change-Id: I013cdc6028a4de3ca826e5ffe30cb8fb33029b64

--- vlan_provider_network/scripts/13/overcloud_deploy.sh
+++ vlan_provider_network/scripts/13/overcloud_deploy.sh
@@ -5,9 +5,9 @@ openstack overcloud deploy \
     --templates /usr/share/openstack-tripleo-heat-templates \
     --libvirt-type kvm \
     --stack overcloud \
-    -r /home/stack/vlan_provider_network/roles/roles_data.yaml \
+    -r /home/stack/vlan_provider_network/roles/roles_data-13.yaml \
     -n /home/stack/vlan_provider_network/network/network-config.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
     -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
     -e /home/stack/vlan_provider_network/network/network-environment-overrides.yaml \
-    -e /home/stack/vlan_provider_network/roles/nodes.yaml
+    -e /home/stack/vlan_provider_network/roles/nodes-13.yaml
